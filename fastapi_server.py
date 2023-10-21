f391249e326eea78afb176a2ae11bdffa612b6ae
sohamtiwari3120 <soham.tiwari800@gmail.com>
2023-10-21 16:23:35 -0400

    fix: backup 10/21

--- fastapi_server.py
+++ fastapi_server.py
@@ -1,9 +1,10 @@
 import uvicorn
 import asyncio
 from typing import List
-import zmq, msgpack
 import traceback
+import json
 import logging
+from zmq_utils import *
 logging.basicConfig(filename='app.log', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 logging.info('starting')
 
@@ -32,16 +33,16 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
-def readFrame(input):
-    [topic, payload] = input.recv_multipart()
-    message = msgpack.unpackb(payload, raw=True)
-    frame = message[b"message"]
-    originatingTime = message[b"originatingTime"]
-    return (frame, originatingTime)
 
 input = zmq.Context().socket(zmq.SUB)
 input.setsockopt_string(zmq.SUBSCRIBE, u"face-orientation")
-input.connect("tcp://127.0.0.1:30002")
+input.connect("tcp://127.0.0.1:40002")
+
+output_sock = create_socket(ip_address="tcp://*:41000")
+
+# input2 = zmq.Context().socket(zmq.SUB)
+# input2.setsockopt_string(zmq.SUBSCRIBE, u"agent-response")
+# input2.connect("tcp://127.0.0.1:30002")
 
 
 @app.websocket("/ws/{client_id}")
@@ -49,13 +50,26 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
     await manager.connect(websocket)
     try:
         while True:
+            # waiting from fe, when it says a text or "hello I am rachel", to send to psi
+            print(f"Waitingg for FE")
+            data = await websocket.receive_text()
+            print(f"Message received from FE {json.loads(data)}")
+            send_payload(output_sock, "fe-tts", data, originatingTime=None)
+
+            # waiting for input from psi to send to fe
             print(f"[{client_id}] Waiting for zeromq input...")
             frame, originatingTime = readFrame(input)
             print(f"Received from PSI: {frame.decode()}, {originatingTime}")
             message = f"{{\"flag\":{1}, \"message\":\"{frame.decode()}\"}}"
+            # message = f"{{\"flag\":{1}, \"message\":\"{'alright alright alright'}\"}}"
+            # frame, originatingTime = readFrame(input2)
+            # print(f"Received from PSI (agent response): {frame.decode()}, {originatingTime}")
+            # message2 = f"{{\"flag\":{1}, \"message\":\"{frame.decode()}\"}}"
             # if type(eval(message)) == str:
             # if type(message) == str
             await manager.send_personal_message(message, websocket)
+            
+            # await manager.send_personal_message(message2, websocket)
             await asyncio.sleep(0.01)
 
     except Exception as e:
@@ -65,4 +79,4 @@ async def websocket_endpoint(websocket: WebSocket, client_id: int):
         await manager.broadcast(f"Client #{client_id} left the chat")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8081)
--- kill_old_server_port.sh
+++ kill_old_server_port.sh
@@ -1,2 +1,3 @@
 pidd="`ps aux | grep -v grep | grep fastapi_server | awk '{print $2}'`"
+echo $pidd
 kill -9 $pidd
